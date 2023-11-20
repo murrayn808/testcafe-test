db75f31ebb0f2b22918fc1173bf888ff380b3147
Nathaniel Murray <murrayn@hawaii.edu>
2023-11-19 22:18:14 -1000

    done

--- app/tests/navbar.component.js
+++ app/tests/navbar.component.js
@@ -52,6 +52,10 @@ class NavBar {
     await testController.click('#login-dropdown');
     await testController.click('#login-dropdown-sign-up');
   }
+
+  async gotoListStuffPage(testController) {
+    await testController.click('#list-stuff-nav');
+  }
 }
 
 export const navBar = new NavBar();
